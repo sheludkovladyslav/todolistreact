cc85c8b5a699d1bed0c39bc26a5c5e5681935760
Vladyslav Sheludko <128414119+sheludkovladyslav@users.noreply.github.com>
2025-11-15 10:01:57 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { AppBar } from "./components/AppBar/AppBar";
+import { Layout } from "./components/Layout/Layout";
+import { TaskForm } from "./components/TaskForm/TaskForm";
+import { TaskList } from "./components/TaskList/TaskList";
+
+function App() {
+  return (
+    <Layout>
+      <AppBar></AppBar>
+      <TaskForm></TaskForm>
+      <TaskList></TaskList>
+    </Layout>
+  );
+}
+
+export default App;
